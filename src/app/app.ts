8691f04f7bfeffb2a0a85f482a82490232ecd535
frank.gairal <frank.gairal@fullsix.com>
2016-09-08 15:27:26 +0700

    feat: move jquery and bootstrap outside of Typescript file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 /// <reference path="../../typings/index.d.ts" />
-import jQuery = require("jquery");
-window['jQuery'] = window['$'] = jQuery;
-import Bootstrap = require("bootstrap");
-[jQuery, Bootstrap];
+declare var $: any;
 
 import { Calculator } from './calculator/calculator';
 
